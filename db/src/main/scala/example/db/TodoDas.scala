55aa8a9b305501a64fd985a7118d949a1c039db0
Junichi Kato <j5ik2o@gmail.com>
2016-09-23 03:57:44 +0900

    refactor in progress.

--- db/src/main/scala/example/db/TodoDas.scala
+++ db/src/main/scala/example/db/TodoDas.scala
@@ -10,22 +10,25 @@ import scala.concurrent.ExecutionContext
 class TodoDas(todoDao: TodoDao) {
 
   def findById(id: TodoId): Source[Todo, NotUsed] =
-    Source.single(id).flatMapConcat { id => Source.fromPublisher(todoDao.resolveAsStream(id)) }
+    Source.fromPublisher(todoDao.resolveAsStream(id))
 
   def findAll: Source[Todo, NotUsed] =
     Source.fromPublisher(todoDao.resolveAllAsStream)
 
-  def create(todo: Todo)(implicit ec: ExecutionContext): Source[Unit, NotUsed] =
+  def create(todo: Todo)
+            (implicit ec: ExecutionContext): Source[Unit, NotUsed] =
     Source.fromFuture {
       todoDao.create(todo)
     }
 
-  def update(id: TodoId, text: String, updateAt: ZonedDateTime, version: Long)(implicit ec: ExecutionContext): Source[Unit, NotUsed] =
+  def update(id: TodoId, text: String, updateAt: ZonedDateTime, version: Long)
+            (implicit ec: ExecutionContext): Source[Unit, NotUsed] =
     Source.fromFuture {
       todoDao.update(id, text, updateAt, version)
     }
 
-  def delete(id: TodoId)(implicit ec: ExecutionContext): Source[Unit, NotUsed] =
+  def delete(id: TodoId)
+            (implicit ec: ExecutionContext): Source[Unit, NotUsed] =
     Source.single(id).mapAsync(1) { id =>
       todoDao.delete(id)
     }
